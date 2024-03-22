f3fda027aa034a4e01b862a9ed6e4da379caab4c
MaxineRaynor <brittanycoles37a644196425@outlook.com>
2024-03-22 13:16:00 +0800

    sapiente

---
+++ index.ts
@@ -0,0 +1,14 @@
+
+export {Client, Result} from "./src/client"
+
+export {Utf8} from "./src/utf8"
+
+export type {Event, ErrorEvent, WebSocketConstructor, WebSocketInterface, CloseEvent, MessageEvent} from "./src/connection"
+
+export {Connection} from "./src/connection"
+
+export type {Option} from "./src/option"
+
+export {RequestTimeout, ConnectTimeout, WebSocket} from "./src/option"
+
+export {ConnError} from "./src/connerror"
